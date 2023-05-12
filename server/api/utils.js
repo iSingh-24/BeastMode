8061804c4464f3a5784207a038e1cd618646d9ea
Inderprit Singh <singhinderprit1992@gmail.com>
2023-05-12 15:41:06 -0400

    made user and workout utils for the backend

---
+++ server/api/utils.js
@@ -0,0 +1,62 @@
+const User = require('../db/models/User');
+const Workout = require('../db/models/Workout');
+
+//user utils
+
+export const getUsers = async () => {
+    try {
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+export const getSingleUser = async (userId) => {
+    try {
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+export const updateUser = async (payload) => {
+    try {
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+export const deleteUser = async (userId) => {
+    try {
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+//workout utils
+
+export const getWorkouts = async () => {
+    try {
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+export const getSingleWorkout = async (workoutId) => {
+    try {
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+export const updateWorkout = async (payload) => {
+    try {
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+export const deleteWorkout = async (workoutId) => {
+    try {
+    } catch (err) {
+        console.log(err);
+    }
+};
