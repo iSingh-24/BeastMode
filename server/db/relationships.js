664fb5f654d88f5a03ceee7161233adcde0e057d
Inderprit Singh <singhinderprit1992@gmail.com>
2023-05-12 01:46:47 -0400

    took out exports from relationships

--- server/db/relationships.js
+++ server/db/relationships.js
@@ -1,2 +1,5 @@
 const User = require('./models/User');
 const Workout = require('./models/Workout');
+
+//set up relationships here and just require the file
+//it is best to probably set it up this way in order to prevent any circular dependencies
